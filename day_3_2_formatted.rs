ba67fcfb8e16438d7c62102581c9ac3df597acd5
th0rex <c0untlizzi@gmail.com>
2022-12-03 12:44:35 +0100

    Update formatted version

--- day_3_2_formatted.rs
+++ day_3_2_formatted.rs
@@ -1,32 +1,21 @@
+use std::{collections::HashSet as H, iter::FromIterator};
 fn main() {
     print!(
         "{}",
-        /*r#"vJrwpWtwJgWrhcsFMMfFFhFp
-        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-        PmmdzqPrVvPwwTWBwg
-        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-        ttgJtRGJQctTZtZT
-        CrZsJsPPZsGzwwsLwLmpwMDw"#*/
         include_str!("3")
             .lines()
             .collect::<Vec<_>>()
             .chunks(3)
             .fold(0, |a, x| {
-                use std::collections::HashSet as H;
-                use std::iter::FromIterator;
-                let f = *H::<char>::from_iter(
-                    H::<char>::from_iter(x[0].chars())
+                let f = *H::<_>::from_iter(
+                    H::<_>::from_iter(x[0].chars())
                         .intersection(&H::from_iter(x[1].chars()))
                         .cloned(),
                 )
                 .intersection(&H::from_iter(x[2].chars()))
                 .next()
                 .unwrap() as u8;
-                a + if f > b'a' {
-                    f - b'a' + 1
-                } else {
-                    f - b'A' + 27
-                } as u64
+                a + if f > 97 { f - 96 } else { f - 38 } as u64
             })
     );
 }
